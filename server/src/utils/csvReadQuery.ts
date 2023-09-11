516f1ae5aefa9faac3f33d2894bd7c47e4f7adce
Harkins12345 <michaelhmeloy@gmail.com>
2023-09-11 03:32:47 +0100

    feat/refactor csv functionality

--- server/src/utils/csvReadQuery.ts
+++ server/src/utils/csvReadQuery.ts
@@ -7,14 +7,16 @@ import { QueryArgs, QueryResult } from "./types";
  */
 export class CSVReadQuery {
   args: QueryArgs;
-  results: [string] | [];
+  csvFilePath: string;
 
   /**
    * Create a new CSV Query instance.
    * @param {QueryArgs} args - Query arguments for filtering results.
+   * @param {string} csvFilePath - Path to folder containing CSV files.
    */
-  constructor(args: QueryArgs) {
+  constructor(args: QueryArgs, csvFilePath: string) {
     this.args = args;
+    this.csvFilePath = csvFilePath;
   }
 
   /**
@@ -58,7 +60,7 @@ export class CSVReadQuery {
    * @returns {QueryResult[]} - Filtered query results
    */
   public getProducts = async () => {
-    const results = await this.readCSV("./data/product.csv");
+    const results = await this.readCSV(`${this.csvFilePath}/product.csv`);
     if (!Object.keys(this.args).length) {
       return results;
     } else {
@@ -71,7 +73,7 @@ export class CSVReadQuery {
    * @returns {QueryResult} - Filtered query result
    */
   public getProduct = async () => {
-    const results = await this.readCSV("./data/product.csv");
+    const results = await this.readCSV(`${this.csvFilePath}/product.csv`);
     return results.filter((result) => result.vin === this.args.vin)[0];
   };
 
@@ -81,7 +83,7 @@ export class CSVReadQuery {
    * @returns {QueryResult[]} - Filtered query results
    */
   public getCustomers = async () => {
-    const results = await this.readCSV("./data/customer.csv");
+    const results = await this.readCSV(`${this.csvFilePath}/customer.csv`);
     if (!Object.keys(this.args).length) {
       return results;
     } else {
@@ -94,7 +96,7 @@ export class CSVReadQuery {
    * @returns {QueryResult} - Filtered query result
    */
   public getCustomer = async () => {
-    const results = await this.readCSV("./data/customer.csv");
+    const results = await this.readCSV(`${this.csvFilePath}/customer.csv`);
     return results.filter((result) => result.email !== this.args.email)[0];
   };
 }
